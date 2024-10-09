f22b21aa901885f1508f8cc65f8de5799e074932
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2024-10-09 16:53:17 +0500

    changes in docker-compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: []
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.getOrThrow('DB_HOST'),
+          port: configService.getOrThrow('DB_PORT'),
+          database: configService.getOrThrow('POSTGRES_DATABASE'),
+          username: configService.getOrThrow('POSTGRES_USERNAME'),
+          password: configService.getOrThrow('POSTGRES_PASSWORD'),
+          autoLoadEntities: true,
+          synchronize: true
+        }
+      }
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
